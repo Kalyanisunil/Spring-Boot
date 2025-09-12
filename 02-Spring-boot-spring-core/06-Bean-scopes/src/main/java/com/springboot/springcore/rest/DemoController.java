b516635d82be004739c4c4107c97d0c1f172d6c5
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-09-12 20:16:03 +0530

    Bean scopes

---
+++ 02-Spring-boot-spring-core/06-Bean-scopes/src/main/java/com/springboot/springcore/rest/DemoController.java
@@ -0,0 +1,41 @@
+package com.springboot.springcore.rest;
+
+import com.springboot.springcore.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    //define private fields for dependency
+    private Coach myCoach;
+    private Coach anotherCoach;
+
+    @Autowired
+    //bean name should have the first letter lowercased
+    //CricketCoach-> bean name =cricketCoach
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                          @Qualifier("cricketCoach") Coach thisanotherCoach) {
+        System.out.println("in constructor:"
+                +getClass().getSimpleName());
+        myCoach = theCoach;
+        anotherCoach=thisanotherCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout()
+    {
+        return myCoach.getDailyWorkOut();
+    }
+
+    @GetMapping("/check")
+    public String check()
+    {
+        return "Comapring beans: myCoach == anotherCoach:"+ (myCoach==anotherCoach);
+    }
+
+
+}
+
+
